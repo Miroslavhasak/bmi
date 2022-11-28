d0b2e52574a03d28e81464262cdeeb5d5b34d2f0
Miroslav Hasak <miro.hasak@gmail.com>
2022-11-28 15:20:45 +0100

    first commit

---
+++ client.c
@@ -0,0 +1,219 @@
+#include <sys/socket.h>  
+#include <netinet/in.h>  
+#include <stdio.h>  
+#include <string.h>  
+#include <stdlib.h> 
+#include <arpa/inet.h> 
+#include <unistd.h> 
+
+main() 
+{  
+    char buf[100];
+    char b1[100];
+    char b2[100];
+    char b3[100];
+    char b4[100];
+    char meno[] = "Som klient";
+    int cislo_klienta;
+    gets(b1);
+    gets(b2);
+    gets(b3);
+    gets(b4);
+
+    int fd1[2];
+    int fd2[2];
+    int fd3[2];
+    int fd4[2];
+
+    pipe(fd1);
+    pipe(fd2);
+    pipe(fd3);
+    pipe(fd4);
+
+    write(fd1[1],b1,100);
+    write(fd2[1],b2,100);
+    write(fd3[1],b3,100);
+    write(fd4[1],b4,100);
+
+    pid_t pid1 = fork();
+    pid_t pid2 = fork();
+
+    
+    //if (pid1 == 0 && 0 == pid2)
+    {
+        printf("vitaj client1\n");
+    }
+
+    /*****************************VYTVORENIE SOCKETU****************************/
+    int sock_desc = socket(AF_INET, SOCK_STREAM, 0); 
+    if (sock_desc == -1)
+    {
+        printf("cannot create socket!\n");
+        return 0;
+    }
+
+    /****************************NASTAVENIE SOCKETU****************************/
+    struct sockaddr_in client;  
+    memset(&client, 0, sizeof(client));  
+    client.sin_family = AF_INET;  
+    client.sin_addr.s_addr = inet_addr("127.0.0.1");
+    client.sin_port = htons(6838);  
+
+    /*****************************KLIENT 1*************************************/
+    
+    if (pid1 == 0 && 0 == pid2)
+    {
+        read(fd1[0],buf,100);
+        cislo_klienta = 1;
+        printf("\n%s %d\n",meno, cislo_klienta);
+          
+    }
+
+    /******************************KLIENT 2************************************/
+
+    if (pid1 > 0 && 0 == pid2)
+    {
+        read(fd2[0],buf,100);
+        cislo_klienta = 2;
+        printf("\n%s %d\n",meno, cislo_klienta);
+        //client.sin_port = htons(6837); 
+    }
+
+    /*******************************KLIENT 3***********************************/
+    
+    if (pid1 == 0 && 0 < pid2)
+    {
+        read(fd3[0],buf,100);
+        cislo_klienta = 3;
+        printf("\n%s %d\n",meno, cislo_klienta);
+        //client.sin_port = htons(6836);
+    }
+
+    /********************************KLIENT 4**********************************/
+
+    if (pid1 > 0 && 0 < pid2)
+    {
+        read(fd4[0],buf,100);
+        cislo_klienta = 4;
+        printf("\n%s %d\n",meno, cislo_klienta);
+        //client.sin_port = htons(6835);
+    }
+
+    /******************************PRIPOJENIE SOCKETU*************************/
+    if (connect(sock_desc, (struct sockaddr*)&client, sizeof(client)) != 0)
+    {
+        printf("\n%s %d;",meno, cislo_klienta);
+        printf("cannot connect to server!\n");
+        close(sock_desc);
+    }
+
+    /*
+    int Second_sock_desc = socket(AF_INET, SOCK_STREAM, 0);
+    if (Second_sock_desc == -1)
+    {
+        printf("\n%s %d;",meno, cislo_klienta);
+        printf("cannot create socket!\n");
+        return 0;
+    }
+    */
+
+    /*******************************NASTAVENIE SOCKETU************************/
+
+    /*
+    struct sockaddr_in server;  
+    memset(&server, 0, sizeof(server));  
+    server.sin_family = AF_INET;
+    server.sin_addr.s_addr = INADDR_ANY;  
+    server.sin_port = htons(16838);  
+    if (bind(Second_sock_desc, (struct sockaddr*)&server, sizeof(server)) != 0)
+    {
+        printf("\n%s %d;",meno, cislo_klienta);
+        printf("cannot bind socket!\n");
+        close(Second_sock_desc);  
+        return 0;
+    }
+    	
+    if (listen(Second_sock_desc, 20) != 0)
+    {
+        printf("\n%s %d;",meno, cislo_klienta);
+        printf("cannot listen on socket!\n");
+        close(Second_sock_desc);  
+        return 0;
+    }
+
+    struct sockaddr_in Second_client;  
+    memset(&Second_client, 0, sizeof(Second_client));  
+    socklen_t Second_len = sizeof(Second_client); 
+    int temp_sock_desc = accept(Second_sock_desc, (struct sockaddr*)&Second_client, &Second_len);  
+    if (temp_sock_desc == -1)
+    {
+        printf("\n%s %d;",meno, cislo_klienta);
+        printf("cannot accept client!\n");
+        close(Second_sock_desc);  
+        return 0;
+    }
+    */
+
+    
+    char c = '\n';
+    char *p_buf;
+    int k, len;  
+
+    while(1) 
+    {      
+        printf("Client: ");
+        //gets(buf);
+
+        len = strlen(buf);
+        p_buf = buf;
+  
+        while (len > 0)
+        {
+            k = send(sock_desc, p_buf, len, 0);  // posielanie dat    
+            if (k == -1)
+            {
+                printf("cannot write to server!\n");
+                break;
+            }
+
+            p_buf += k;
+            len -= k;
+        }
+
+        k = send(sock_desc, &c, 1, 0);      
+        if (k == -1)
+        {
+            printf("cannot write to server!\n");
+            break;
+        }
+
+        if (strcmp(buf, "exit") == 0)          
+            break;  
+	    k = recv(sock_desc, buf, 100, 0);      
+        if (recv == -1)
+        {
+            printf("\ncannot read from client!\n");
+            break;
+        }
+
+        if (recv == 0)
+        {
+            printf("\nclient disconnected.\n");
+            break;
+        }
+
+        if (k > 0)          //tu je vystup
+            printf("Server: %*.*s", k, k, buf); 
+        if (strcmp(buf, "exit") == 0)
+            break;
+
+    break;
+    }
+
+ 
+
+    close(sock_desc);  // zatvorenie socketu
+    printf("client disconnected\n");
+
+    return 0;  
+}
---
+++ server.c
@@ -0,0 +1,462 @@
+#include <sys/socket.h>  
+#include <netinet/in.h>  
+#include <stdio.h>  
+#include <string.h> 
+#include <stdlib.h>  
+#include <arpa/inet.h> 
+#include <unistd.h> 
+
+char *body_mass_index(char *buffer) {
+    float vyska_cm, vaha, bmi, vyska_m, vyska_2;
+    int a = 0, b = 0;
+    char odpoved[100] = "Tvoje bmi je: ", cislo[50], ch_vyska[50], ch_hmotnost[50];  
+
+    memset(ch_vyska, 0, sizeof(ch_vyska));
+    while (buffer[a] != ' ')
+    {
+        ch_vyska[a] = buffer[a];
+        a++;
+    }
+
+    a = a + 1;
+    while (buffer[a] != '\0')
+    {
+        ch_hmotnost[b] = buffer[a];
+        b++;
+        a++;
+    }
+    vyska_cm = atof(ch_vyska);       //atof premiena zo stringu na double 
+    vaha = atof(ch_hmotnost);       //premenim si z charu na float
+
+    /*
+    printf("\nvyska_cm je>%f",vyska_cm);
+    printf("\nvaha je>%f",vaha);
+    printf("\nch_vyska je>%s",ch_vyska);
+    printf("\nch_hmotnost je>%s",ch_hmotnost);
+
+    printf("Zadaj tvoju vysku v cm a vahu v kg dodrz poradie!!!: \n");
+    */
+    //premena vysky z cm na m
+    vyska_m = vyska_cm / 100.00;
+    //printf("\nupravena ultra gbiga vyska_m je>%f",vyska_m);
+
+    //vyska na druhu
+    vyska_2 = vyska_m * vyska_m;
+    //printf("\nvyska_2 je>%f",vyska_2);
+    //vypocet bmi
+    bmi = (vaha)/(vyska_2);
+    //printf("\nbmi je>%f",bmi);
+
+    memset(buffer, 0, sizeof(buffer));
+    memset(ch_hmotnost, 0, sizeof(ch_hmotnost));
+    memset(ch_vyska, 0, sizeof(ch_vyska));
+
+    sprintf(cislo, "%.2f", bmi);
+    strcpy(buffer, odpoved);
+    strcat(buffer, cislo);
+
+    /*
+    printf("\nodpoved vo funkcii je: %s", odpoved);
+    printf("\ncislo vo funkcii je: %s", cislo);*/
+    //printf("\nbuffer vo funkcii je: %s\n", buffer);
+    
+    return buffer;
+}
+
+int main() 
+{  
+
+    float bmi;
+    char len;
+    char buf[100];  
+    int k;  
+    
+    /*****************************VYTVORENIE SOCKETU***************************/
+
+    int sock_desc = socket(AF_INET, SOCK_STREAM, 0);
+    char c = '\n';
+    char *p_buf, *save_buffer, *send_msg;
+
+    if (sock_desc == -1)
+    {
+        printf("cannot create socket!\n");
+        return 0;
+    }
+
+    /****************************NASTAVENIE SOCKETU****************************/
+
+    struct sockaddr_in server; 
+
+    memset(&server, 0, sizeof(server));  
+    server.sin_family = AF_INET;
+    server.sin_addr.s_addr = INADDR_ANY;  
+    server.sin_port = htons(6838); 
+
+    if (bind(sock_desc, (struct sockaddr*)&server, sizeof(server)) != 0)
+    {
+        printf("cannot bind socket!\n");
+        close(sock_desc);  
+        return 0;
+    }
+
+    /****************************LISTEN SERVER SOCKET**************************/
+
+    int listen_socket;
+
+    listen_socket = listen(sock_desc, 20);
+
+    if (listen_socket == 0)
+    {
+        printf("Waiting for clients\n");
+    }
+
+    else {
+        printf("Cannot listen on socket!\n");
+        close(sock_desc);  
+        return 0;
+    }
+
+    
+    
+    /*********************************1 CLIENT*****************************/
+
+    sleep(5);
+    int client1 = accept(sock_desc, NULL, NULL);  
+    if (client1 == -1)
+    {
+        printf("cannot accept client!\n");
+        close(sock_desc);  
+        return 0;
+    }
+
+    /*********************************2 CLIENT*****************************/
+    
+
+    int client2 = accept(sock_desc, NULL, NULL);  
+    if (client2 == -1)
+    {
+        printf("cannot accept client!\n");
+        close(client1);
+        close(sock_desc);
+        return 0;
+    }
+
+    /*********************************3 CLIENT*****************************/
+
+
+    int client3 = accept(sock_desc, NULL, NULL);  
+    if (client3 == -1)
+    {
+        printf("cannot accept client!\n");
+        close(client1); 
+        close(client2);
+        close(sock_desc);  
+        return 0;
+    }
+    /*********************************4 CLIENT*****************************/
+
+
+    int client4 = accept(sock_desc, NULL, NULL);  
+    if (client4 == -1)
+    {
+        printf("cannot accept client!\n");
+        close(client1);
+        close(client2); 
+        close(client3); 
+        close(sock_desc);  
+        return 0;
+    }
+
+    /*
+    sleep(1);
+    
+	int Second_sock_desc = socket(AF_INET, SOCK_STREAM, 0); 
+    if (Second_sock_desc == -1)
+    {
+        printf("cannot create socket!\n");
+        return 0;
+    }
+
+    */
+    /******************************NASTAVENIE SOCKETU**************************/
+    /*
+    struct sockaddr_in Second_client;  
+    memset(&Second_client, 0, sizeof(Second_client));  
+    Second_client.sin_family = AF_INET;  
+    Second_client.sin_addr.s_addr = inet_addr("127.0.0.1");  
+    Second_client.sin_port = htons(16838);  
+    */
+
+    /********************************PRIPOJENIE SOCKETU************************/
+    /*
+    if (connect(Second_sock_desc, (struct sockaddr*)&Second_client, sizeof(Second_client)) != 0)
+    {
+        printf("cannot connect to server!\n");
+        close(Second_sock_desc);
+    }
+    */
+
+	printf("Client sa pripojil\n");
+
+    pid_t pid1 = fork();
+    pid_t pid2 = fork();
+        
+    /***********************************CLIENT 1********************************/
+    
+    if (pid1 == 0 && 0 == pid2)
+    {
+
+    while(1) 
+    {    
+        k = recv(client1, buf, 100, 0);
+
+        if (recv == -1)
+        {
+            printf("\ncannot read from client!\n");
+            break;
+        }
+
+        if (recv == 0)
+        {
+            printf("\nclient disconnected.\n");
+            break;
+        }
+
+        if (k > 0)          //tu je vystup
+            printf("Client: %*.*s", k, k, buf);
+            save_buffer = body_mass_index(buf);
+            //printf("\nmuhahah buffer je: %s ", save_buffer);
+
+        if (strcmp(buf, "exit") == 0)         
+            break;     
+	
+	printf("Server: ");
+    //gets(buf);
+
+        len = strlen(save_buffer);
+        p_buf = save_buffer;
+
+        while (len > 0)
+        {
+            k = send(client1, save_buffer, len, 0);  // posielanie dat    
+            if (k == -1)
+            {
+                printf("cannot write to server!\n");
+                break;
+            }
+
+            p_buf += k;
+            len -= k;
+        }
+
+        k = send(client1, &c, 1, 0);      
+        if (k == -1)
+        {
+            printf("cannot write to server!\n");
+            break;
+        }
+
+        
+
+        if (strcmp(buf, "exit") == 0)          
+            break;  
+        
+    break;
+    }
+    }
+
+    /**********************************CLIENT 2*********************************/
+
+    if (pid1 > 0 && 0 < pid2)
+    {
+
+    while(1) 
+    {    
+        k = recv(client2, buf, 100, 0);
+        
+        if (recv == -1)
+        {
+            printf("\ncannot read from client!\n");
+            break;
+        }
+
+        if (recv == 0)
+        {
+            printf("\nclient disconnected.\n");
+            break;
+        }
+
+        if (k > 0)          //tu je vystup
+            printf("Client: %*.*s", k, k, buf);
+            save_buffer = body_mass_index(buf);
+            //printf("\nmuhahah buffer je: %s ", save_buffer);
+
+        if (strcmp(buf, "exit") == 0)         
+            break;     
+	
+	printf("Server: ");
+    //gets(buf);
+
+        len = strlen(save_buffer);
+        p_buf = save_buffer;
+
+        while (len > 0)
+        {
+            k = send(client2, save_buffer, len, 0);  // posielanie dat    
+            if (k == -1)
+            {
+                printf("cannot write to server!\n");
+                break;
+            }
+
+            p_buf += k;
+            len -= k;
+        }
+
+        k = send(client2, &c, 1, 0);      
+        if (k == -1)
+        {
+            printf("cannot write to server!\n");
+            break;
+        }
+
+        
+
+        if (strcmp(buf, "exit") == 0)          
+            break; 
+    break;
+    }
+    }
+
+    /*****************************CLIENT 3**************************************/
+    
+    if (pid1 == 0 && 0 < pid2)
+    {
+
+    while(1) 
+    {    
+        k = recv(client3, buf, 100, 0);
+        
+        if (recv == -1)
+        {
+            printf("\ncannot read from client!\n");
+            break;
+        }
+
+        if (recv == 0)
+        {
+            printf("\nclient disconnected.\n");
+            break;
+        }
+
+        if (k > 0)          //tu je vystup
+            printf("Client: %*.*s", k, k, buf);
+            save_buffer = body_mass_index(buf);
+            //printf("\nmuhahah buffer je: %s ", save_buffer);
+
+        if (strcmp(buf, "exit") == 0)         
+            break;     
+	
+	printf("Server: ");
+    //gets(buf);
+
+        len = strlen(save_buffer);
+        p_buf = save_buffer;
+
+        while (len > 0)
+        {
+            k = send(client3, save_buffer, len, 0);  // posielanie dat    
+            if (k == -1)
+            {
+                printf("cannot write to server!\n");
+                break;
+            }
+
+            p_buf += k;
+            len -= k;
+        }
+
+        k = send(client3, &c, 1, 0);      
+        if (k == -1)
+        {
+            printf("cannot write to server!\n");
+            break;
+        }
+        
+        if (strcmp(buf, "exit") == 0)          
+            break;  
+    break;
+    }
+    }
+
+    /**************************************CLIENT 4****************************/
+    
+    if (pid1 > 0 && 0 == pid2)
+    {
+
+   while(1) 
+    {    
+        k = recv(client4, buf, 100, 0);
+        
+        if (recv == -1)
+        {
+            printf("\ncannot read from client!\n");
+            break;
+        }
+
+        if (recv == 0)
+        {
+            printf("\nclient disconnected.\n");
+            break;
+        }
+
+        if (k > 0)          //tu je vystup
+            printf("Client: %*.*s", k, k, buf);
+            save_buffer = body_mass_index(buf);
+            //printf("\nmuhahah buffer je: %s ", save_buffer);
+
+        if (strcmp(buf, "exit") == 0)         
+            break;     
+	
+	printf("Server: ");
+    //gets(buf);
+
+        len = strlen(save_buffer);
+        p_buf = save_buffer;
+
+        while (len > 0)
+        {
+            k = send(client4, save_buffer, len, 0);  // posielanie dat    
+            if (k == -1)
+            {
+                printf("cannot write to server!\n");
+                break;
+            }
+
+            p_buf += k;
+            len -= k;
+        }
+
+        k = send(client4, &c, 1, 0);      
+        if (k == -1)
+        {
+            printf("cannot write to server!\n");
+            break;
+        }
+
+
+        if (strcmp(buf, "exit") == 0)          
+            break;  
+    break;
+    }
+    }
+
+    close(client1);  
+    close(client2);
+    close(client3);
+    close(client4);
+    close(sock_desc);  
+    //close(Second_sock_desc);  
+    printf("server disconnected\n");
+    return 0;  
+}
